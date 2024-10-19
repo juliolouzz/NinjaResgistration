39c5c47861d38832a4c5b0dc2e45d9012d37d884
juliolouzz <julio.cdl.vet@gmail.com>
2024-10-19 18:28:52 +0100

    first commit

---
+++ src/test/java/dev/java10x/ninjaregistration/NinjaRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.ninjaregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
