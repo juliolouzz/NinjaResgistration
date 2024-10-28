ea9dfc924a055833b271319bd2d77c7f69516c61
juliolouzz <jlouz.dev@gmail.com>
2024-10-28 17:50:39 +0000

    feat: Create files of NinjaService, NinjaRepository and MissionsRepository

---
+++ src/main/java/dev/java10x/ninjaregistration/Missions/MissionsRepository.java
@@ -0,0 +1,6 @@
+package dev.java10x.ninjaregistration.Missions;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissionsRepository extends JpaRepository<MissionsModel, Long> {
+}
