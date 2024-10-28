c0ced9ea378f7bfb7b7de89876ab5616188c3c4e
juliolouzz <jlouz.dev@gmail.com>
2024-10-28 12:38:20 +0000

    fix: On email column, sets UNIQUE constraint to true

--- src/main/java/dev/java10x/ninjaregistration/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/ninjaregistration/Ninjas/NinjaModel.java
@@ -20,8 +20,12 @@ public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
+    @Column(unique = true)
     private String email;
+
     private int age;
 
     // @ManyToOne -> Many Ninjas to One Mission
