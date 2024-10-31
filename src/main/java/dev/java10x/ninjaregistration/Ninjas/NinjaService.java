18bbf6bf0dbf84fe8ececc56d8a4ac19378bebbd
juliolouzz <jlouz.dev@gmail.com>
2024-10-31 17:57:28 +0000

    fix: Includes @JsonIgnore on `MissionsModel` annotation to prevent serialization error

--- src/main/java/dev/java10x/ninjaregistration/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/ninjaregistration/Ninjas/NinjaService.java
@@ -1,4 +1,22 @@
 package dev.java10x.ninjaregistration.Ninjas;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class NinjaService {
+
+
+    private NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    // List all ninjas
+    public List<NinjaModel> showAllNinjas() {
+        return ninjaRepository.findAll();
+    }
+
 }
